d2cf35fc44f23ef05aac51f27b81278a04f10534
MaksBurenko <94311611+MaksBurenko@users.noreply.github.com>
2023-12-01 13:08:47 +0300

    123

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,23 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         int i = 0;
-        int p = 1;
+        long p = 1;
+        try {
+            //       Scanner scanner = new Scanner(System.in);
+            Scanner scanner = new Scanner("2000000000");
+            long n = scanner.nextLong();
 
-        //       Scanner scanner = new Scanner(System.in);
-        Scanner scanner = new Scanner("5");
-
-        int n = scanner.nextInt();
-
-        while (p < n){
-            i++;
-            p = p*2;
+            while (p < n){
+                i++;
+                p = p*2;
+            }
+            System.out.println(i);
+        }catch (Exception e){
+            e.printStackTrace(System.out);
         }
-        System.out.println(i);
     }
 }
